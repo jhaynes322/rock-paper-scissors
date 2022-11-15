4c25b8d1b9fb28f98bac86706503a41a104ef43f
Jared Haynes <jared.haynes@charter.net>
2022-11-15 13:48:58 -0600

    Re-added js

---
+++ script.js
@@ -0,0 +1,58 @@
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3 + 1);
+    if (num === 1) {
+        return "Rock";
+    } else if (num === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let userScore = 0;
+    let computerScore = 0;
+    let = playerSelection = prompt("Make your move");
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
+        ++computerScore;
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Rock") {
+        return "It's a tie. Go Again";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
+        ++userScore;
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
+        ++userScore;
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Paper") {
+        return "Its a tie. Go again";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
+        ++computerScore;
+        return "You Lose. Scissors beat Paper";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
+        ++computerScore;
+        return "You Lose. Rock beats Scissors";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
+        ++userScore;
+        return "You Win! Scissor beat Paper";
+    } else {
+        return "Its a tie. Go Again";
+    }
+
+}
+
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
+let playerSelection = prompt("Pick a move");
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let userScore = 0;
+        let computerScore = 0;
+        let playerSelection = prompt("Pick a move");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection))
+        console.log("your score = " + userScore);
+        console.log("Computer's score = " + computerScore);
+    }
+}
